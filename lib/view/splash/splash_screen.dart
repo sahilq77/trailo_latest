f858c54dff3d4dd1e0ae73ca016c9668cfa29eec
sahilq77 <sahil.lekawale@quickensol.in>
2026-02-12 17:30:40 +0530

    app vesrion

--- lib/view/splash/splash_screen.dart
+++ lib/view/splash/splash_screen.dart
@@ -1,6 +1,7 @@
 import 'dart:developer';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:package_info_plus/package_info_plus.dart';
 import '../../controller/splash/splash_controller.dart';
 import '../../utility/app_colors.dart';
 
@@ -12,14 +13,19 @@ class SplashScreen extends StatefulWidget {
 }
 
 class _SplashScreenState extends State<SplashScreen> {
+  String appVersion = '';
+
   @override
   void initState() {
-    // TODO: implement initState
     super.initState();
+    _getAppVersion();
+  }
 
-    // Future.delayed(Duration(seconds: 3), () {
-    //   Get.offNamed(AppRoutes.welcome);
-    // });
+  Future<void> _getAppVersion() async {
+    final packageInfo = await PackageInfo.fromPlatform();
+    setState(() {
+      appVersion = packageInfo.version;
+    });
   }
 
   @override
@@ -29,8 +35,26 @@ class _SplashScreenState extends State<SplashScreen> {
     final screenHeight = MediaQuery.of(context).size.height;
     return Scaffold(
       backgroundColor: AppColors.primary,
-      body: Center(
-        child: Icon(Icons.medical_services, size: 100, color: AppColors.white),
+      body: Stack(
+        children: [
+          Center(
+            child: Icon(
+              Icons.medical_services,
+              size: 100,
+              color: AppColors.white,
+            ),
+          ),
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Padding(
+              padding: const EdgeInsets.only(bottom: 30),
+              child: Text(
+                'v$appVersion',
+                style: TextStyle(color: AppColors.white, fontSize: 14),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
